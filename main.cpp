7341e5ede4540f142ef59fbe653159203383539f
Иван Комиссаров <i.komissarov@2Gis.ru>
2016-06-03 15:25:11 +0300

    Better word counting based on GNU algoritm

--- main.cpp
+++ main.cpp
@@ -10,17 +10,17 @@ struct WordCount
 	WordCount(const WordCount &other) = default;
 	WordCount &operator=(const WordCount &other) = default;
 
-	int chars = 0;
-	int words = 0;
-	int lines = 0;
+	int charCount = 0;
+	int wordCount = 0;
+	int lineCount = 0;
 };
 
 WordCount operator+ (const WordCount &lhs, const WordCount &rhs)
 {
 	WordCount result = lhs;
-	result.chars += rhs.chars;
-	result.words += rhs.words;
-	result.lines += rhs.lines;
+	result.charCount += rhs.charCount;
+	result.wordCount += rhs.wordCount;
+	result.lineCount += rhs.lineCount;
 	return result;
 }
 
@@ -34,14 +34,30 @@ WordCount handleStream(std::istream &input_stream)
 	WordCount result;
 	char c = 0;
 
-	while (input_stream.get(c)) {
-		if (c == ' ')
-			result.words++;
-		else if (c == '\n')
-			result.words++;
-		else
-			result.chars++;
-	}
+	do {
+		while (input_stream.get(c)) {
+			if (isalpha(c)) {
+				result.wordCount++;
+				break;
+			}
+
+			result.charCount++;
+			if (c == '\n')
+				result.lineCount++;
+		}
+
+		while (input_stream) {
+			result.charCount++;
+			if (c == '\n')
+				result.lineCount++;
+
+			if (isspace(c) || c == '\n') {
+				break;
+			}
+			input_stream.get(c);
+		};
+
+	} while (input_stream);
 
 	return result;
 }
@@ -75,7 +91,7 @@ int main(int argc, char *argv[])
 			}
 		}
 
-		std::cout << totalCount.lines << "\t" << totalCount.words << "\t" << totalCount.chars << std::endl;
+		std::cout << totalCount.lineCount << "\t" << totalCount.wordCount << "\t" << totalCount.charCount << std::endl;
 
 	} catch (const std::exception &ex) {
 		std::cerr << ex.what() << std::endl;
