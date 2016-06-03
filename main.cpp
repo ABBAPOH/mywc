1c3349732b03a3bbd03c836922a7ff22b8d7affd
Иван Комиссаров <i.komissarov@2Gis.ru>
2016-06-03 16:23:17 +0300

    Added options

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #include <iostream>
 #include <fstream>
@@ -26,7 +27,21 @@ WordCount operator+ (const WordCount &lhs, const WordCount &rhs)
 
 struct ParsedArguments
 {
+	ParsedArguments() = default;
+	ParsedArguments(const ParsedArguments &other) = default;
+	ParsedArguments &operator=(const ParsedArguments &other) = default;
+
+	enum Flags {
+		NoFlags = 0,
+		CharCount = 0x1,
+		WordCount = 0x2,
+		LineCount = 0x4,
+		DefaultMask = 0x7,
+		Help = 0x8,
+	};
+
 	std::vector<std::string> files;
+	int flags = NoFlags;
 };
 
 WordCount handleStream(std::istream &input_stream)
@@ -69,21 +84,58 @@ WordCount handleFile(const std::string &fileName)
 	return handleStream(file);
 }
 
-ParsedArguments parse(int argc, char *argv[])
+ParsedArguments parseArguments(int argc, char *argv[])
 {
 	ParsedArguments result;
 
-	if (argc > 1) {
-		for (int i = 1; i < argc; ++i)
-			result.files.push_back(argv[i]);
+	int opt = 0;
+	while ((opt = getopt(argc, argv, "mlwh")) != -1) {
+		switch (opt) {
+		case 'm':
+			result.flags |= ParsedArguments::Flags::CharCount;
+			break;
+		case 'l':
+			result.flags |= ParsedArguments::Flags::LineCount;
+			break;
+		case 'w':
+			result.flags |= ParsedArguments::Flags::WordCount;
+			break;
+		case 'h':
+			result.flags |= ParsedArguments::Flags::Help;
+			break;
+		default:
+			break;
+		}
 	}
+
+	if ((result.flags & ParsedArguments::Flags::DefaultMask) == 0)
+		result.flags |= ParsedArguments::Flags::DefaultMask;
+
+	for (int i = optind; i < argc; ++i)
+		result.files.push_back(argv[i]);
 
 	return result;
 }
 
-void printCount(const WordCount &count, const std::string &fileName = std::string())
+void printCount(const WordCount &count, int flags, const std::string &fileName = std::string())
 {
-	std::cout << count.lineCount << "\t" << count.wordCount << "\t" << count.charCount;
+	using Flags = ParsedArguments::Flags;
+
+	if (flags & Flags::LineCount)
+		std::cout << count.lineCount;
+
+	if (flags & Flags::WordCount) {
+		if (flags & Flags::LineCount)
+			std::cout << "\t";
+		std::cout << count.wordCount;
+	}
+
+	if (flags & Flags::CharCount) {
+		if (flags & Flags::LineCount || flags & Flags::CharCount)
+			std::cout << "\t";
+		std::cout << count.charCount;
+	}
+
 	if (!fileName.empty())
 		std::cout << "\t" << fileName;
 	std::cout << std::endl;
@@ -92,24 +144,30 @@ void printCount(const WordCount &count, const std::string &fileName = std::strin
 int main(int argc, char *argv[])
 {
 	try {
-		auto parsed = parse(argc, argv);
+
+		auto parsed = parseArguments(argc, argv);
+
+		if (parsed.flags & ParsedArguments::Flags::Help) {
+			std::cout << "Usage: " << "mywc" << " [-m] [-l] [-w] [FILE]..." << std::endl;
+			return 1;
+		}
 
 		if (parsed.files.empty()) {
 			auto count = handleStream(std::cin);
-			printCount(count);
+			printCount(count, parsed.flags);
 		} else if (parsed.files.size() == 1) {
 			const auto fileName = parsed.files.front();
 			auto count = handleFile(fileName);
-			printCount(count, fileName);
+			printCount(count, parsed.flags, fileName);
 		} else {
 			WordCount totalCount;
 			for (auto fileName : parsed.files) {
 				const auto count = handleFile(fileName);
 				totalCount = totalCount + count;
 
-				printCount(count, fileName);
+				printCount(count, parsed.flags, fileName);
 			}
-			printCount(totalCount, "total");
+			printCount(totalCount, parsed.flags, "total");
 		}
 
 	} catch (const std::exception &ex) {
